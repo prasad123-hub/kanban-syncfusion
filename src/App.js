85773134787c67d6f8ae719c46579b105718de4d
Prasad <narkhedkarprasad@gmail.com>
2022-02-09 23:15:33 +0530

    kanban

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+import { extend } from "@syncfusion/ej2-base";
+import data from "./dataSource.json";
+import "./App.css";
 
 function App() {
+  const dataSource = extend([], data.kanbanData, null, true);
+  console.log("data", data.kanbanData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <KanbanComponent
+        id="kanban"
+        keyField="Status"
+        dataSource={dataSource}
+        cardSettings={{ contentField: "Summary", headerField: "Id" }}
+        swimlaneSettings={{ keyField: "Assignee" }}
+      >
+        <ColumnsDirective>
+          <ColumnDirective headerText="To Do" keyField="Open" />
+          <ColumnDirective headerText="In Progress" keyField="InProgress" />
+          <ColumnDirective headerText="Testing" keyField="Testing" />
+          <ColumnDirective headerText="Done" keyField="Close" />
+        </ColumnsDirective>
+      </KanbanComponent>
+    </>
   );
 }
 
---
+++ src/datasource.js
@@ -0,0 +1,22 @@
+export const kanbandata = [
+  {
+    Id: 1,
+    Summary: "Analyze the new requirements gathered from the customer",
+  },
+  {
+    Id: 2,
+    Summary: "Analyze the new requirements gathered from the customer",
+  },
+  {
+    Id: 3,
+    Summary: "Analyze the new requirements gathered from the customer",
+  },
+  {
+    Id: 4,
+    Summary: "Analyze the new requirements gathered from the customer",
+  },
+  {
+    Id: 5,
+    Summary: "Analyze the new requirements gathered from the customer",
+  },
+];
